0f79a1d902967fc330228d81ef46e6fe667f99f0
udinocera <udinocera@gmail.com>
2023-11-13 17:31:17 +0100

    fine 3 esercizio

---
+++ output.mjs
@@ -0,0 +1,5 @@
+function output(year){
+    console.log(`Umberto è nato nel ${year}`);
+}
+
+export default output
---
+++ script.mjs
@@ -0,0 +1,5 @@
+import output from "./output.mjs";
+
+const year = 2002
+
+output(year)
