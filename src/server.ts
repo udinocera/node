6bde1f1afa66d9498f34cc0d1ae7556d0d2fc990
udinocera <udinocera@gmail.com>
2023-11-28 21:40:24 +0100

    fine esercizio

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,44 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from "express"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import 'express-async-errors'
 
-console.log(TODO);
+dotenv.config();
+
+type Planet = {
+    id: number,
+    name: string,
+  };
+
+  type Planets = Planet[];
+
+  let planets: Planets = [
+    {
+      id: 1,
+      name: "Earth",
+    },
+    {
+      id: 2,
+      name: "Mars",
+    },
+  ];
+
+  const app = express();
+
+  app.use(express.json());
+  app.use(morgan('dev'));
+  
+  app.get('/planets', (req, res) => {
+    res.json(planets);
+  });
+  
+  app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
+  
+  const PORT = process.env.PORT || 3000;
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
