6b742779a89154682dcabdae34c2ca9a5c48670e
udinocera <udinocera@gmail.com>
2023-12-15 02:33:54 +0100

    correzione

--- src/server.ts
+++ src/server.ts
@@ -1,62 +1,56 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import 'express-async-errors';
-import Joi from 'joi';
-import multer from "multer"
-import {getAll, getOneById, create, updateById, deleteById, createImage} from "./controllers/planets"
-import { logIn, signUp } from './controllers/users';
-dotenv.config();
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import multer from "multer";
 
-const app = express();
-
-app.use(express.json());
-app.use(morgan('dev'));
-
-const planetSchema = Joi.object({
-  name: Joi.string().required(),
-});
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+  createImage
+} from "./controllers/planets.js";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./uploads")
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    cb(null, file.originalname)
   }
 })
 const upload = multer({ storage })
 
-const validatePlanet = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const { error } = planetSchema.validate(req.body);
-  if (error) {
-    return res.status(400).json({ error: error.details[0].message });
-  }
-  next();
-};
+dotenv.config();
 
-app.get('/api/planets', getAll );
-
-app.get('/api/planets/:id', getOneById);
-
-app.post('/api/planets', validatePlanet, create);
-
-app.put('/api/planets/:id', validatePlanet, updateById);
-
-app.delete('/api/planets/:id', deleteById);
-
-app.post("/api/planets/:id/image", upload.single("image"), createImage)
+const app = express();
+const port = process.env.PORT;
 
 app.use("/uploads", express.static("uploads"))
 
-app.use("/static", express.static("static"))
+app.use(express.json());
+app.use(morgan("dev"));
 
-app.post("/api/users/login", logIn)
 
-app.post("/api/users/signup", signUp)
+app.get("/api/planets", getAll);
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/api/planets/:id", getOneById);
+
+
+app.post("/api/planets", create);
+
+
+app.put("/api/planets/:id", updateById);
+
+
+app.delete("/api/planets/:id", deleteById);
+
+
+app.post("/api/planets/:id/image", upload.single("image"), createImage)
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:3000`);
 });
