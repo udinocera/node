7b5165b66872ef83f7eb635ea0a39aa89248e3e0
udinocera <udinocera@gmail.com>
2023-12-15 02:16:31 +0100

    fine esercizio

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -1,82 +1,66 @@
 import { Request, Response } from "express";
-import Joi from "joi";
 import pgPromise from "pg-promise";
 
-const db = pgPromise()("postgres://postgres:Forzanapoli1@@localhost:5432/postgres");
+const db = pgPromise()("postgres://postgres:postgres@localhost:5432/planets");
 
 const setupDb = async () => {
-  await db.none("DROP TABLE IF EXISTS planets");
   await db.none(`
-    CREATE TABLE planets(
+    DROP TABLE IF EXISTS planets;
+
+    CREATE TABLE planets (
       id SERIAL NOT NULL PRIMARY KEY,
-      name TEXT NOT NULL
-    )
+      name TEXT NOT NULL,
+      image TEXT
+    );
   `);
 
   await db.none(`INSERT INTO planets (name) VALUES ('Earth')`);
   await db.none(`INSERT INTO planets (name) VALUES ('Mars')`);
-  await db.none(`INSERT INTO planets (name) VALUES ('Saturn')`);
 
-  const planets = await db.many(`SELECT * FROM planets`);
-
-  console.log(planets);
+  const planets = await db.many(`SELECT * FROM planets;`);
 };
-
 setupDb();
 
-
-const planetSchema = Joi.object({
-  name: Joi.string().required(),
-});
-
-
 const getAll = async (req: Request, res: Response) => {
-  const planets = await db.many(`SELECT * FROM planets`);
-
-  res.status(200).json(planets);
+  const planets = await db.many(`SELECT * FROM planets;`);
+  res.json(planets);
 };
-
 
 const getOneById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const planet = await db.one(`SELECT * FROM planets WHERE id=$1;`, Number(id));
-
-  res.status(200).json(planet);
+  const planetId = parseInt(req.params.id, 10);
+  const planet = await db.oneOrNone(`SELECT * FROM planets WHERE id=$1`, planetId);
+  res.json(planet);
 };
-
 
 const create = async (req: Request, res: Response) => {
   const { name } = req.body;
   const newPlanet = { name };
-  const validateNewPlanet = planetSchema.validate(newPlanet);
-
-  if (validateNewPlanet.error) {
-    return res
-      .status(400)
-      .json({ msg: validateNewPlanet.error.details[0].message });
-  } else {
-    await db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
-    res.status(201).json({ msg: "The planet was created" });
-  }
+  await db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
+  res.status(201).json({ msg: "Planet created successfully" });
 };
-
 
 const updateById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name]);
-
-  res.status(200).json({ msg: "The planet was updated" });
+  const {id} = req.params
+  const {name} = req.body
+  await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name])
+  res.json({ msg: "Planet updated successfully" });
 };
-
 
 const deleteById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  await db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
-
-  res.status(200).json({ msg: "The planet was deleted" });
+  const planetId = req.params.id;
+  await db.none(`DELETE FROM planets WHERE id=$1`, Number(planetId))
+  res.json({ msg: "Planet deleted successfully" });
 };
 
-export { getAll, getOneById, create, updateById, deleteById };
+const createImage = async (req: Request, res: Response) => {
+  const {id} = req.params
+  const fileName = req.file?.path
+  if (fileName) {
+    db.none(`UPDATE planets SET image=$2 WHERE id=$1`, [id, fileName])
+    res.status(201).json({msg: "Planet image uploaded successfully."})
+  } else {
+    res.status(400).json({msg: "Planet image failed to uploaded."})
+  }
+}
+
+export { getAll, getOneById, create, updateById, deleteById, createImage };
--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,57 @@
-import express from "express";
-import "express-async-errors";
-import dotenv from "dotenv";
-import morgan from "morgan";
-
-import {
-  getAll,
-  getOneById,
-  create,
-  updateById,
-  deleteById,
-} from "./controllers/planets.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import 'express-async-errors';
+import Joi from 'joi';
+import multer from "multer"
+import {getAll, getOneById, create, updateById, deleteById, createImage} from "./controllers/planets"
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
+const planetSchema = Joi.object({
+  name: Joi.string().required(),
+});
 
-app.get("/api/planets", getAll);
-
-
-app.get("/api/planets/:id", getOneById);
-
-
-app.post("/api/planets", create);
-
-
-app.put("/api/planets/:id", updateById);
-
-
-app.delete("/api/planets/:id", deleteById);
-
-
-app.listen(3000, () => {
-  console.log("the server is running on http://localhost:3000");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads")
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
 })
+const upload = multer({ storage })
+
+const validatePlanet = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const { error } = planetSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
+
+app.get('/api/planets', getAll );
+
+app.get('/api/planets/:id', getOneById);
+
+app.post('/api/planets', validatePlanet, create);
+
+app.put('/api/planets/:id', validatePlanet, updateById);
+
+app.delete('/api/planets/:id', deleteById);
+
+app.post("/api/planets/:id/image", upload.single("image"), createImage)
+
+app.use("/uploads", express.static("uploads"))
+app.use("/static", express.static("static"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
