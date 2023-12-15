c6a046d5c53f61f52dbde5e588930ed92f5c0f24
udinocera <udinocera@gmail.com>
2023-12-15 02:32:46 +0100

    fine

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import 'express-async-errors';
 import Joi from 'joi';
 import multer from "multer"
 import {getAll, getOneById, create, updateById, deleteById, createImage} from "./controllers/planets"
-
+import { logIn, signUp } from './controllers/users';
 dotenv.config();
 
 const app = express();
@@ -48,7 +48,12 @@ app.delete('/api/planets/:id', deleteById);
 app.post("/api/planets/:id/image", upload.single("image"), createImage)
 
 app.use("/uploads", express.static("uploads"))
+
 app.use("/static", express.static("static"))
+
+app.post("/api/users/login", logIn)
+
+app.post("/api/users/signup", signUp)
 
 const PORT = process.env.PORT || 3000;
 
